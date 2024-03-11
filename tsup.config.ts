3b2a04c3759668d037f8d4fcc7b795fb37f74164
phothinmg <phothinmg@gmail.com>
2024-03-11 17:43:39 +0630

    first commit

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: [
+    "index.css",
+  ],
+  outDir: "./dist/umoe",
+  // splitting: false,
+  sourcemap: true,
+  clean: true,
+  // dts: true,
+  // format: "esm",
+  bundle: true,
+  loader: {
+    ".png": "dataurl",
+    ".woff": "dataurl",
+    ".woff2": "dataurl",
+    ".eot": "dataurl",
+    ".ttf": "dataurl",
+    ".svg": "dataurl",
+    ".otf": "dataurl",
+  },
+  minify: true,
+});
